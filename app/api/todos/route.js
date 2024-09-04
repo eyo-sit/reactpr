840e19f2a072266dd2eb84927e9cffa934c840a1
ebsitota <ebsitota@email.wm.edu>
2024-09-04 12:50:01 -0400

    add csr ssr ssg prototypes

---
+++ app/api/todos/route.js
@@ -0,0 +1,6 @@
+export async function GET(){
+	// connecting to db
+	// look up todos of this user
+	//
+	return Response.json({messeage: "this is your data from the server"})
+}
--- app/page.tsx
+++ app/page.tsx
@@ -1,18 +1,20 @@
+export const revalidate = 60
 
 const fetchTodos = async () => {
-	const res = await fetch("https://jsonplaceholder.typicode.com/todos");
-	const data = await res.json();
-	return data;
+	const res = await fetch("https://jsonplaceholder.typicode.com/todos")
+	const data = await res.json()
+	return data
 }
 
 export default async function Home() {
-	const todos = await fetchTodos();	
+	const todos = await fetchTodos()
 	return (
 		<main>
 			<h1>Statically generated</h1>
-			{todos.map((todo: any) => (
+			{todos.map((todo) => (
 				<p key={todo.id}>{todo.title}</p>
 			))}
 		</main>
-  	)
+	)
 }
+
